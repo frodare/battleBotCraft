a53a24e10a85d65a3e1c1799aed87272e7d70071
Charles Howard <cdhoward@add123.com>
2016-11-24 12:14:43 -0500

    fixed typo

--- java/com/frodare/battlebotcraft/BattleBotCraft.java
+++ java/com/frodare/battlebotcraft/BattleBotCraft.java
@@ -16,7 +16,7 @@ public class BattleBotCraft {
 	public static final String VERSION = "1.10.2-1";
 	public static final String MODNAME = "BattleBotCraft";
 
-	@SidedProxy(clientSide = "com.frodare.battlebotcraftt.ClientProxy", serverSide = "com.frodare.battlebotcraft.ServerProxy")
+	@SidedProxy(clientSide = "com.frodare.battlebotcraft.ClientProxy", serverSide = "com.frodare.battlebotcraft.ServerProxy")
 	public static CommonProxy proxy;
 
 	@Instance(value = BattleBotCraft.MODID)
