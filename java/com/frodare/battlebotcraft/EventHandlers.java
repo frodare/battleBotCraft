0b8eb60e9cee774a1ccf09e616b1b1da3ff6fa00
Charles Howard <cdhoward@add123.com>
2016-11-25 11:06:46 -0500

    Experimenting with using Rhino to run JavaScript code from within Minecraft

--- java/com/frodare/battlebotcraft/EventHandlers.java
+++ java/com/frodare/battlebotcraft/EventHandlers.java
@@ -5,6 +5,11 @@ import static spark.Spark.get;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.tools.shell.Global;
+
 import com.frodare.battlebotcraft.entities.EntityBattleBot;
 import com.frodare.battlebotcraft.entities.EntityBattleBot.BattleBotEvent;
 
@@ -16,15 +21,16 @@ import spark.Spark;
 
 public class EventHandlers {
 
+	private int count;
 	private Set<EntityBattleBot> entities = new HashSet<>();
 
 	public EventHandlers() {
-		
+		runJS();
 		try {
 			Spark.staticFileLocation("/public");
-			get("/hello", (req, res) -> "Hello World, boo!");
+			get("/hello", (req, res) -> "Hello World");
 			get("/hello/:name", (req, res) -> "Hello: " + req.params(":name"));
-			
+
 			get("/entities", (req, res) -> {
 				System.out.println("/entities");
 				StringBuilder s = new StringBuilder();
@@ -40,15 +46,33 @@ public class EventHandlers {
 		}
 	}
 
+	public void log(String s) {
+		System.out.println("LOG (" + count + "): " + s);
+	}
+
+	private void runJS() {
+		String evaluationScript = "print('TEST!'); BBC.log('TEST FROM JS');";
+		Context cx = Context.enter();
+		try {
+			Scriptable scope = cx.initStandardObjects();
+			Global global = new Global(cx);
+			Object wrappedValue = Context.javaToJS(this, global);
+			ScriptableObject.putProperty(global, "BBC", wrappedValue);
+			cx.evaluateString(global, evaluationScript, "EvaluationScript", 1, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			Context.exit();
+		}
+	}
+
 	@SubscribeEvent
 	public void startServer(WorldEvent.Load e) {
-		System.out.println("*********** world load *** ");
+
 	}
 
 	@SubscribeEvent
 	public void onSpawnEntity(BattleBotEvent.Spawn e) {
-
-		System.out.println("*********** spawn  *** " + e.getEntity().getName());
 		entities.add(e.getEntity());
 	}
 
